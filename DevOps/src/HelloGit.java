e44027ac5e1bc3910b68fe7ce74293b5174291a1
kumudkk <kunjilwark@gmail.com>
2019-06-19 10:32:23 -0500

    Modified and added a line

--- DevOps/src/HelloGit.java
+++ DevOps/src/HelloGit.java
@@ -3,6 +3,9 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git I am here");
+		//This is the line added after the commit
+		
+		System.out.print("After commiting");
 
 	}
 
